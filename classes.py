974e79deaddf14506ab7cdc7fe7a22ef3ecdce6f
Elixerofdoom <elixerofdoom@web.de>
2022-07-22 01:36:37 +0200

    created fn that gives all possible vecs of piece. some testing fn's also

--- classes.py
+++ classes.py
@@ -1,18 +1,12 @@
+from typing import TypeAlias
 
-from re import X
-
+Vector: TypeAlias = tuple[int, int]
 
 class Piece:
     """vec attr = (y, x)"""
-    def __init__(self, color : str):
+    def __init__(self, color: str):
         self.color = color
         self.first_move = False
-
-        #self.pos = pos
-    # OLD: 
-    # def update_pos(self, x, y):
-    #     self.posx = x
-    #     self.posy = y
 
     def __repr__(self):
         return f"Piece('{self.color}')"
@@ -23,7 +17,7 @@ class Piece:
     def moved(self):
         self.first_move = False
 
-    def active_move(self, vec : tuple[int, int]):
+    def active_move(self, vec: tuple[int, int]):
         self.active_vec = vec
 
     def rm_active_vec(self):
@@ -31,38 +25,106 @@ class Piece:
 
  
 class King(Piece):
-    name : str = "K"
-    range : int = 1
-    vec : tuple[tuple[int, int],...] = (1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (-1, 1), (1, -1), (-1, -1),
+    name: str = "K"
+    range: int = 1
+    vec: tuple[Vector,...] = (
+        (1, 0), (-1, 0), (0, 1), (0, -1), 
+        (1, 1), (-1, 1), (1, -1), (-1, -1),
+    )
     
 class Queen(Piece):
-    name : str = "Q"
-    range : int = 7
-    vec : tuple[tuple[int, int],...] = (1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (-1, 1), (1, -1), (-1, -1), 
+    name: str = "Q"
+    range: int = 7
+    vec: tuple[Vector,...] = (
+        (1, 0), (-1, 0), (0, 1), (0, -1), 
+        (1, 1), (-1, 1), (1, -1), (-1, -1), 
+    )
 
 class Bishop(Piece):
-    name : str = "B"
-    range : int = 7
-    vec : tuple[tuple[int, int],...] = (1, 1), (-1, 1), (1, -1), (-1, -1),
+    name: str = "B"
+    range: int = 7
+    vec: tuple[Vector,...] = (
+        (1, 1), (-1, 1), (1, -1), (-1, -1),
+    )
 
 class Rook(Piece):
-    name : str = "R"
-    range : int = 7
-    vec : tuple[tuple[int, int],...] = (1, 0), (-1, 0), (0, 1), (0, -1),
+    name: str = "R"
+    range: int = 7
+    vec: tuple[Vector,...] = (
+    (1, 0), (-1, 0), (0, 1), (0, -1),
+    )
 
 class Pawn(Piece):
-    name : str = "P"
-    range : int = 1
+    name: str = "P"
+    range: int = 1
     def __init__(self, color: str):
         # set vector of pawn piece
         self.color = color
-        if color == 'w': self.vec : tuple[tuple[int, int],...] = (-1, 0),  # (0, 0) are needed because ((-1, 0), (0, 0)) is desired
-        else: self.vec = (1, 0),              # and ((-1,0)) gets reduced to (-1, 0)   
+        if color == 'w': 
+            self.vec: tuple[Vector,...] = ((-1, 0),) 
+        else: 
+            self.vec: tuple[Vector,...] = ((1, 0),)             
 
 # Knight special piece
 class Knight(Piece):
     """ Knight has range 1 so for loops only go once"""
-    name : str = "N"
-    jumps : bool = True
-    range : int = 1
-    vec : tuple[tuple[int, int],...] = (2, 1), (1, 2), (-1, 2), (-2, 1), (-2, -1), (-1, -2), (1, -2), (2, -1)
+    name: str = "N"
+    jumps: bool = True
+    range: int = 1
+    vec: tuple[Vector,...] = (
+        (2, 1), (1, 2), (-1, 2), (-2, 1), 
+        (-2, -1), (-1, -2), (1, -2), (2, -1)
+    )
+
+# ============================================================= #
+#                   TESTING AREA
+
+def get_start_board():
+    # TODO: Rework loading the board and
+    """creates start board"""
+    board = [
+        [
+            Rook('b'), Knight('b'), Bishop('b'), Queen('b'),
+            King('b'), Bishop('b'), Knight('b'), Rook('b')
+        ],
+        [
+            Pawn('b'), Pawn('b'), Pawn('b'), Pawn('b'),
+            Pawn('b'), Pawn('b'), Pawn('b'), Pawn('b')
+        ],
+            ['<>','<>','<>','<>','<>','<>','<>','<>'],                                                              # 6
+            ['<>','<>','<>','<>','<>','<>','<>','<>'],                                                              # 5
+            ['<>','<>','<>','<>','<>','<>','<>','<>'],                                                              # 4
+            ['<>','<>','<>','<>','<>','<>','<>','<>'],                                                              # 3
+        [
+            Pawn('w'), Pawn('w'), Pawn('w'), Pawn('w'),
+            Pawn('w'), Pawn('w'), Pawn('w'), Pawn('w')
+        ],
+        [
+            Rook('w'), Knight('w'), Bishop('w'), Queen('w'),
+            King('w'), Bishop('w'), Knight('w'), Rook('w')
+        ],
+    ]
+    return board
+
+def all_possible_vectors(
+    piece: Piece,
+    board: list[list, list]
+) -> list[tuple[int, int], ]:
+    """return all possible vectors the given piece has on the board"""
+    all_vectors = []
+    for s in range(1, piece.range+1):
+        for v in piece.vec:
+            y = v[0] * s
+            x = v[1] * s
+            try:
+                if board[y][x]:
+                    pass
+            except IndexError:
+                continue
+            all_vectors.append((y, x))
+    return all_vectors
+
+king = King("w")
+board = get_start_board()
+
+av = all_possible_vectors(piece=king, board=board)
--- cli_version.py
+++ cli_version.py
@@ -1,29 +1,36 @@
-from locale import normalize
-from shutil import move
+from typing import TypeAlias
 from classes import Piece, King, Queen, Bishop, Knight, Rook, Pawn
 
 # vars
 padding = '\t\t'
+Vector: TypeAlias = tuple[int, int]
 
 # functions
 def get_start_board():
     # TODO: Rework loading the board and
     """creates start board"""
     board = [
-    [Rook('b'), Knight('b'), Bishop('b'), Queen('b'), King('b'), Bishop('b'), Knight('b'), Rook('b')],      # 8
-    [Pawn('b'), Pawn('b'), Pawn('b'), Pawn('b'), Pawn('b'), Pawn('b'), Pawn('b'), Pawn('b')],               # 7
-    ['<>','<>','<>','<>','<>','<>','<>','<>'],                                                              # 6
-    ['<>','<>','<>','<>','<>','<>','<>','<>'],                                                              # 5
-    ['<>','<>','<>','<>','<>','<>','<>','<>'],                                                              # 4
-    ['<>','<>','<>','<>','<>','<>','<>','<>'],                                                              # 3
-    [Pawn('w'), Pawn('w'), Pawn('w'), Pawn('w'), Pawn('w'), Pawn('w'), Pawn('w'), Pawn('w')],               # 2
-    [Rook('w'), Knight('w'), Bishop('w'), Queen('w'), King('w'), Bishop('w'), Knight('w'), Rook('w')],      # 1
+        [
+            Rook('b'), Knight('b'), Bishop('b'), Queen('b'),
+            King('b'), Bishop('b'), Knight('b'), Rook('b')
+        ],
+        [
+            Pawn('b'), Pawn('b'), Pawn('b'), Pawn('b'),
+            Pawn('b'), Pawn('b'), Pawn('b'), Pawn('b')
+        ],
+            ['<>','<>','<>','<>','<>','<>','<>','<>'],                                                              # 6
+            ['<>','<>','<>','<>','<>','<>','<>','<>'],                                                              # 5
+            ['<>','<>','<>','<>','<>','<>','<>','<>'],                                                              # 4
+            ['<>','<>','<>','<>','<>','<>','<>','<>'],                                                              # 3
+        [
+            Pawn('w'), Pawn('w'), Pawn('w'), Pawn('w'),
+            Pawn('w'), Pawn('w'), Pawn('w'), Pawn('w')
+        ],
+        [
+            Rook('w'), Knight('w'), Bishop('w'), Queen('w'),
+            King('w'), Bishop('w'), Knight('w'), Rook('w')
+        ],
     ]
-    # OLD: used when Piece had attr posx posy
-    # for y, row in enumerate(board):
-    #     for x, item in enumerate(row):
-    #         if isinstance(item, Piece):
-    #             item.update_pos(x, y)
     return board
 
 def draw_board(board):
@@ -39,7 +46,7 @@ def draw_board(board):
                 print(' ' + str(row_num))
                 row_num -= 1
 
-def chess_idx_to_lst(board_pos: str) -> tuple:  
+def chess_idx_to_lst(board_pos: str) -> tuple:
     """Returns -> list index y, x"""
     board_y, board_x =  int(board_pos[1]), board_pos[0]
     match board_x.lower():
@@ -54,9 +61,9 @@ def chess_idx_to_lst(board_pos: str) -> tuple:
 
     # this func turns chess board y index to lst y index
     y = -1 * board_y + 8
-    print(f'{board_pos} | ({y}, {x})') # DEBUG: 
+    print(f'{board_pos} | ({y}, {x})') # DEBUG:
     return y, x
-    
+
 def lst_idx_to_chess(idx):
     lst_y, lst_x = idx
     match lst_x:
@@ -72,42 +79,72 @@ def lst_idx_to_chess(idx):
     y = 8 - lst_y
     return y, x
 
-def check_colinearity(piece : Piece, oy: int, ox: int, ny: int, nx: int) -> bool:
+def check_colinearity(piece: Piece, vec: Vector) -> bool:
     """
-    checks if one vector in piece.vec is colinear to the vector of point piece to point destination.
-    PS: Ask EYDEE how he would do this function. Problem: dont want to use cls method active_move to pseudo return vec
+    checks if one vector in piece.vec is colinear to the vector of
+    point piece to point destination.
+    PS: Ask EYDEE how he would do this function. Problem: dont want
+    to use cls method active_move to pseudo return vec
     """
-    # TODO: Implement Knight moveset
-    vecy = ny - oy
-    vecx = nx - ox
     for scalar in range(1, piece.range+1):
-        vec = (vecy/scalar, vecx/scalar)
+        vec = (vec[0]/scalar, vec[1]/scalar)
         if vec in piece.vec:
-            vec = (int(vecy/scalar), int(vecx/scalar)) # convert it to int because list indices later
+            # convert it to int because list indices later
+            vec = (int(vec[0]/scalar), int(vec[1]/scalar))
             piece.active_move(vec)
             print("vec found! active_move called") # DEBUG
             return True
     print("vec not found! Invalid move") # DEBUG
     return False
-    
-def check_collision(piece : Piece, board : list[list[any]], oy : int, ox : int, ny : int, nx : int) -> bool:
+
+def all_possible_vectors(
+    piece: Piece,
+    board: list[list, list],
+    oy: int, ox: int,
+) -> list[tuple[int, int], ]:
+    """return all possible vectors the given piece has on the board"""
+    all_vectors = []
+    for s in range(1, piece.range+1):
+        for v in piece.vec:
+            y = v[0] * s
+            x = v[1] * s
+            
+            if ox + x < 0:
+                # if x is negative it wraps around list
+                continue
+            try:
+                if board[oy+y][ox+x]:
+                    pass
+            except IndexError:
+                continue
+            all_vectors.append((y, x))
+    return all_vectors
+
+def check_collision(
+    piece: Piece,
+    board: list[list[any]],
+    vec: tuple[int, int],
+    ny: int, nx: int,
+    oy: int, ox: int,
+) -> bool:
     """ check_collision takes the self.active_vec from piece and checks if any pieces are in its way
-        to its destination   
+        to its destination
     """
-    # TODO: find a better way to store the valid vec in the fn check_colineartiy() and giving 
+    # TODO: find a better way to store the valid vec in
+    # the fn check_colineartiy() and giving
     if hasattr(piece, "jumps"):
         piece.rm_active_vec() # not used by the stupid Knight
         if board[ny][nx] == '<>' or board[ny][nx].color != piece.color:
-            
-            return True
-        else: 
-            return False
-        
 
-    vecy, vecx = move_vec = piece.active_vec # eg move_vec = (0, 4) or (3, 3)
+            return True
+        else:
+            return False
+
+    vecy, vecx = piece.active_vec # eg move_vec = (0, 4) or (3, 3)
     piece.rm_active_vec()
     # we just have to iterate the amount of the highest num in move_vec
-    # set comprehension down there gets the highest num from a tuple with 0 in it or with 2 same values
+    # set comprehension down there gets the highest
+    # num from a tuple with 0 in it or with 2 same values
     for stp in range(1, piece.range+1):
         try_y, try_x = oy + (vecy*stp), ox + (vecx*stp)
         tryyx = try_y, try_x
@@ -129,48 +166,81 @@ def check_collision(piece : Piece, board : list[list[any]], oy : int, ox : int, 
             print(f"Captured enemy piece at {lst_idx_to_chess((try_y, try_x))}")
             return True
 
+
+def testfn_fill_vectors(vectors, board, y, x):
+    """ fills all possible tiles where piece can move with XX's """
+    # BUG: Somehow og current_field var gets overwritten 
+    # despite true copying list
+    for vector in vectors:
+        vecy, vecx = vector[0], vector[1]
+        board[y+vecy][x+vecx] = "XX"
+    draw_board(board)
+    input()
+
+
+
+#----------------------------------------------------------------------
+
 def main():
     current_board = get_start_board()
     #breakpoint() #DEBUG
     while True:
         turn_counter = 1 # odd turns white & even turns black
-        while True: 
+        while True:
             # Player Turn actual main loop
             draw_board(current_board)
-            if turn_counter % 2 == 1: current_player = 'White' 
+            if turn_counter % 2 == 1: current_player = 'White'
             else: current_player = 'Black'
-            
-            selected_field = input(f"{current_player}'s Turn: " ) # TODO: Input validation
+
+            # TODO: Input validation
+            selected_field = input(f"{current_player}'s Turn: " )
             y, x = chess_idx_to_lst(selected_field)
-            piece : Piece = current_board[y][x]
+            piece: Piece = current_board[y][x]
+
             # check if right piece is grabbed
             if piece == '<>' or piece.color != current_player[0].lower():
                 print('invalid target') # DEBUG
                 continue
-            
-            destination = input(f'Move {piece} ({selected_field}) to: ') # TODO: Input validation
+            # TODO: Input validation
+            destination = input(f'Move {piece} ({selected_field}) to: ')
             ny, nx = chess_idx_to_lst(destination)
+
             # check move validity
             if y == ny and x == nx:
                 continue
-            if not check_colinearity(piece, y, x, ny, nx):
-                print(f"{piece} on {selected_field} can't move to {destination}") # DEBUG
-                continue
-            if not check_collision(piece, current_board, y, x, ny, nx):
-                continue
+            vecy = ny - y
+            vecx = nx - x
+            vec = (vecy, vecx)
 
-            current_board[ny][nx], current_board[y][x] = current_board[y][x], current_board[ny][nx]
+            av = all_possible_vectors(piece, current_board, y, x)
             
-            draw_board
+
+            test_board = current_board[:]
+            testfn_fill_vectors(av, test_board, y, x)
+
+
+            # TODO: UNCOMMENT 
+            # if not check_colinearity(piece, vec,):
+            #     print(
+            #         f"{piece} on {selected_field} can't move to {destination}"
+            #     ) # DEBUG
+            #     continue
+
+            # if not check_collision(piece, current_board, vec, ny, nx):
+            #     continue
+
+            # current_board[ny][nx], current_board[y][x] = current_board[y][x], current_board[ny][nx]
+            # TODO: UNCOMMENT
+
             turn_counter += 1
 
-        # check where piece can move 
+        # check where piece can move
         # give options where to move
         # check if move was valid
         # check if new pos is occupied
         # move piece
         # check for win
-        
+
         break
 
 if __name__ == "__main__":
