657584f266b205a121743c16579989d74ccbf6ba
Elixerofdoom <elixerofdoom@web.de>
2022-07-22 20:38:44 +0200

    sync commit

--- cli_version.py
+++ cli_version.py
@@ -5,7 +5,7 @@ from classes import Piece, King, Queen, Bishop, Knight, Rook, Pawn
 # vars
 padding = '\t\t'
 Vector: TypeAlias = tuple[int, int]
-ChessArray: TypeAlias = list[list[Piece | str], list[Piece | str],]
+ChessArray: TypeAlias = list[list[Piece | str],]
 
 # functions
 def get_start_board() -> ChessArray:
@@ -20,10 +20,10 @@ def get_start_board() -> ChessArray:
             Pawn('b'), Pawn('b'), Pawn('b'), Pawn('b'),
             Pawn('b'), Pawn('b'), Pawn('b'), Pawn('b')
         ],
-            ['<>','<>','<>','<>','<>','<>','<>','<>'],                                                              # 6
-            ['<>','<>','<>','<>','<>','<>','<>','<>'],                                                              # 5
-            ['<>','<>','<>','<>','<>','<>','<>','<>'],                                                              # 4
-            ['<>','<>','<>','<>','<>','<>','<>','<>'],                                                              # 3
+            ['<>','<>','<>','<>','<>','<>','<>','<>'],
+            ['<>','<>','<>','<>','<>','<>','<>','<>'],                                                              
+            ['<>','<>','<>','<>','<>','<>','<>','<>'],
+            ['<>','<>','<>','<>','<>','<>','<>','<>'],
         [
             Pawn('w'), Pawn('w'), Pawn('w'), Pawn('w'),
             Pawn('w'), Pawn('w'), Pawn('w'), Pawn('w')
@@ -35,8 +35,8 @@ def get_start_board() -> ChessArray:
     ]
     return board
 
-def draw_board(board):
-    """print out chess board"""
+def draw_board(board) -> None:
+    """print out chess board in a nice way"""
     print(end='\n\n\n')
     print( padding + ' A  B  C  D  E  F  G  H')
     row_num = 8
@@ -49,7 +49,7 @@ def draw_board(board):
                 row_num -= 1
 
 def chess_idx_to_lst(board_pos: str) -> tuple:
-    """Returns -> list index y, x"""
+    """Converts chess board index to list index"""
     board_y, board_x =  int(board_pos[1]), board_pos[0]
     match board_x.lower():
         case 'a': x = 0
@@ -61,12 +61,13 @@ def chess_idx_to_lst(board_pos: str) -> tuple:
         case 'g': x = 6
         case 'h': x = 7
 
-    # this func turns chess board y index to lst y index
+    # this func turns chess board y index to list y index
     y = -1 * board_y + 8
-    print(f'{board_pos} | ({y}, {x})') # DEBUG:
+    print(f'{board_pos} | ({y}, {x})')          # DEBUG:
     return y, x
 
-def lst_idx_to_chess(idx) -> str:
+def lst_idx_to_chess(idx: tuple[int, int]) -> str:
+    """Reverse function to chess_idx_to_list()"""
     lst_y, lst_x = idx
     match lst_x:
         case 0: x = 'a'
@@ -82,30 +83,27 @@ def lst_idx_to_chess(idx) -> str:
     return f"{x.upper()}{y}"
 
 def check_colinearity(piece: Piece, vec: Vector) -> bool:
-    """
-    checks if one vector in piece.vec is colinear to the vector of
+    """checks if one vector in piece.vec is colinear to the vector of
     point piece to point destination.
     PS: Ask EYDEE how he would do this function. Problem: dont want
-    to use cls method active_move to pseudo return vec
-    """
+    to use cls method active_move to pseudo return vec"""
+
     for scalar in range(1, piece.range+1):
-        vec = (vec[0]/scalar, vec[1]/scalar)
+        vec = (vec[0] / scalar, vec[1] / scalar)
         if vec in piece.vec:
             # convert it to int because list indices later
-            vec = (int(vec[0]/scalar), int(vec[1]/scalar))
-            print("vec found!") # DEBUG
+            vec = (int(vec[0] / scalar), int(vec[1] / scalar))
+            print("vec found!")         # DEBUG
             return True
-    print("vec not found! Invalid move") # DEBUG
+    print("vec not found! Invalid move")        # DEBUG
     return False
 
 def all_possible_vectors(
-    piece: Piece,
-    board: list[list, list],
-    oy: int, ox: int,
-) -> list[tuple[int, int], ]:
+    piece: Piece, board: ChessArray, oy: int, ox: int,
+    ) -> tuple[Vector,...]:
     """return all possible vectors the given piece has on the board"""
     all_vectors = []
-    for s in range(1, piece.range+1):
+    for s in range(1, piece.range + 1):
         for v in piece.vec:
             y = v[0] * s
             x = v[1] * s
@@ -114,60 +112,95 @@ def all_possible_vectors(
                 # if x is negative it wraps around list
                 continue
             try:
-                if board[oy+y][ox+x]:
+                if board[oy + y][ox + x]:
                     pass
             except IndexError:
                 continue
             all_vectors.append((y, x))
-    return all_vectors
+    return tuple(all_vectors)
 
 def check_collision(
-    piece: Piece,
-    board: list[list[any]],
+    piece: Piece, 
+    board: ChessArray,
     vec: Vector,
     ny: int, nx: int,
     oy: int, ox: int,
 ) -> bool:
-    """ check_collision takes the self.active_vec from piece and checks if any pieces are in its way
-        to its destination
-    """
+    """ takes given vec and checks if any piece is between that origin 
+        to destination. Knight move is special case """
+    # TODO: THIS WHOLE FUNCTION IS SHIT 
+    # SEPERATE IT INTO MORE PARTS!!!
+
+
+    # Check Special Knight Case first
     if hasattr(piece, "jumps"):
-        if board[ny][nx] == '<>' or board[ny][nx].color != piece.color:
-            print(f"{piece} no collision {board[ny][nx]}") # DEBUG
+        if (board[ny][nx] == '<>' 
+                or board[ny][nx].color != piece.color):              
+            print(f"{piece} no collision {board[ny][nx]}")      # DEBUG
             return True
         else:
-            print(f"{piece} collison on {board[ny][nx]}") # DEBUG
+            print(f"{piece} collison on {board[ny][nx]}")       # DEBUG
             return False
 
+
+    # whoops seems like i made a mistak with the given vec. 
+    # try_y and try_x are wrong coz 
+    # given vec is the distance between the origin and destini
+    # and not the vectors of the piece 
     vecy, vecx = vec
     for s in range(1, piece.range+1):
-        try_y, try_x = oy + (vecy*s), ox + (vecx*s)
+        try_y, try_x = oy + (vecy * s), ox + (vecx * s)
         tryyx = try_y, try_x
         newpos = ny, nx
-        # next step empty AND NOT destination -> NEXT TILE
-        if board[try_y][try_x] == '<>' and tryyx != newpos:
+
+        # for formatting
+        chess_idx =lst_idx_to_chess((try_y, try_x))
+        on_tile = board[try_y][try_x]
+
+        if (on_tile == '<>' 
+                and tryyx != newpos):
+            # next step empty AND NOT destination 
+            # -> NEXT TILE
             continue
-        # next step a piece AND NOT destination -> BLOCKED
-        if board[try_y][try_x] != '<>' and tryyx != newpos:
-            print(f"the path is blocked by {board[try_y][try_x]} at {lst_idx_to_chess((try_y, try_x))}") # DEBUG
-            return False # BLOCKED
-        # next step empty AND destination -> REACHED DESTINATION
-        elif board[try_y][try_x] == '<>' and tryyx == newpos:
-            print(f"REACHED DESTINTY at {lst_idx_to_chess((try_y, try_x))}") # DEBUG
+
+        if (on_tile != '<>' 
+                and tryyx != newpos):
+            # next step a piece AND NOT destination 
+            # -> BLOCKED
+            print(          
+                f"""the path is blocked by {on_tile} at {chess_idx}"""
+            )       # DEBUG
+            return False
+
+        
+        elif (on_tile == '<>' 
+                and tryyx == newpos):
+            print(f"REACHED DESTINTY at {chess_idx}")        # DEBUG
+            # if next tile empty AND destination 
+            # -> REACHED DESTINATION       
             return True
-        # next step a piece AND destination AND piece has diff color -> REACHED DESTINATION AND KILLS ENEMY
-        elif board[try_y][try_x] != '<>' and try_y == ny and try_x == ny and board[try_y][try_x].color != piece.color:
-            print(f"Captured enemy piece at {lst_idx_to_chess((try_y, try_x))}")
+
+        elif (on_tile != '<>' 
+                and try_y == ny 
+                and try_x == ny 
+                and on_tile.color != piece.color):
+            # next tile a piece AND destination AND piece has diff color 
+            # -> REACHED DESTINATION AND CAPS ENEMY
+            print(f"Captured enemy piece at {chess_idx}")       # DEBUG
             return True
 
 
-def testfn_fill_vectors(vectors, board, y, x):
+def testfn_fill_vectors(
+    vectors: tuple[Vector,...], 
+    board: ChessArray, 
+    y: int, x: int,
+    ) -> None:
     """ fills all possible tiles where piece can move with XX's """
     # BUG: Somehow og current_field var gets overwritten 
     # despite true copying list
     for vector in vectors:
         vecy, vecx = vector[0], vector[1]
-        board[y+vecy][x+vecx] = "XX"
+        board[y + vecy][x + vecx] = "XX"
     draw_board(board)
     input()
 
@@ -176,7 +209,8 @@ def testfn_fill_vectors(vectors, board, y, x):
 def main():
     current_board = get_start_board()
     while True:
-        turn_counter = 1 # odd White even Black
+        turn_counter = 1            # odd White even Black
+        
         while True:
             # Current Player Turn
             draw_board(current_board)
@@ -184,42 +218,44 @@ def main():
             else: current_player = 'Black'
 
             # TODO: Input validation
-            selected_field = input(f"{current_player}'s Turn: " )
-            y, x = chess_idx_to_lst(selected_field)
+            selected_tile = input(f"{current_player}'s Turn: " )
+            y, x = chess_idx_to_lst(selected_tile)
+            piece: Piece = current_board[y][x]
+            
             if current_board[y][x] == '<>':
-                print(f"{selected_field} is empty")
+                print(f"{selected_tile} is empty")
                 continue
+
             if current_board[y][x].color != current_player[0].lower():
-                print(f"{current_player} that piece does not belong to you!")
+                print(f"{current_player}, {piece} does not belong to you!")
                 continue
             
-            piece: Piece = current_board[y][x]
             # TODO: Input validation
-            destination = input(f'Move {piece} ({selected_field}) to: ')
+            destination = input(f'Move {piece} ({selected_tile}) to: ')
             ny, nx = chess_idx_to_lst(destination)
 
             # check move validity
             if y == ny and x == nx:
                 continue
+
             vecy = ny - y
             vecx = nx - x
             vec = (vecy, vecx)
 
             av = all_possible_vectors(piece, current_board, y, x)
 
-
             # BUG: IF you copy with [:] the og list gets overwritten somehow
             # BUG: if copy wiht deepcopy collision check does not work somehow
             # BUG: FKING KILL ME NOW
             # test_board = current_board[:]
-            # test_board = deepcopy(current_board)
-            # testfn_fill_vectors(av, test_board, y, x)
-
+            
+            test_board = deepcopy(current_board)
+            testfn_fill_vectors(av, test_board, y, x)
 
             if not check_colinearity(piece, vec):
                 print(
-                    f"{piece} on {selected_field} can't move to {destination}"
-                ) # DEBUG
+                    f"{piece} on {selected_tile} can't move to {destination}"
+                )       # DEBUG
                 continue
 
             if not check_collision(piece, current_board, 
