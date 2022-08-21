e5e80ce6a8bf47a9efa1fd7c92ad0434acbe2bc7
Deniz Ekren <elixerofdoom@web.de>
2022-08-22 01:08:48 +0200

    now all vectors are calculated at the start

--- cli_version.py
+++ cli_version.py
@@ -5,6 +5,7 @@ from classes import Piece, King, Queen, Bishop, Knight, Rook, Pawn
 # vars
 padding = '\t\t'
 Vector: TypeAlias = tuple[int, int]
+Coordinate: TypeAlias = tuple[int, int]
 ChessArray: TypeAlias = list[list[Piece | str],]
 
 # functions
@@ -52,6 +53,7 @@ def ipt_checker(string) -> bool:
     """ checks if given string matches chess idx"""
     if len(string) != 2:
         return False
+
     if (string.isalnum()
             and not string.isdigit()
             and not string.isalpha()
@@ -59,7 +61,7 @@ def ipt_checker(string) -> bool:
         return True
     else:
         print('Something happend with ipt_checker()')
-        return False
+        return
 
 def chess_idx_to_lst(board_pos: str) -> tuple:
     """ Converts chess board index to list index """
@@ -95,13 +97,26 @@ def lst_idx_to_chess(idx: tuple[int, int]) -> str:
     y = 8 - lst_y
     return f"{x.upper()}{y}"
 
+def belongs_to_player(board: ChessArray, player_color: str, y: int, x: int) -> bool:
+    """ Checks if given item in array belongs to given player."""
+    if board[y][x] == '<>':
+        return False
 
-def all_board_vectors( piece: Piece, board: ChessArray, oy: int, ox: int,
-    ) -> list[list[list[Vector,],]]:
-    """ Returns all possible vectors the given piece has on the empty board
+    elif board[y][x].color != player_color.lower():
+        return False
+
+    elif board[y][x].color == player_color.lower():
+        return True
+
+    else:
+        print("Something happend w/ belongs_to_player()")                   # DEBUG      
+
+def free_vectors(board: ChessArray, piece: Piece, oy: int, ox: int,
+    ) -> dict:
+    """ Returns all possible vectors the given piece has on a empty board
         Return is sorted by directions. Each inner list is is in ascending order. """
     
-    board_vectors = []
+    piece_vectors = {piece: []}
     vectors = piece.my_vectors()
 
     for v in vectors:
@@ -112,19 +127,25 @@ def all_board_vectors( piece: Piece, board: ChessArray, oy: int, ox: int,
             
             if ox + x < 0 or oy + y < 0:
                 # if x is negative it wraps around list
-                continue
+                break
+
+            if ox + x > 7 or oy + y > 7:
+                break
+
             try:
                 if board[oy + y][ox + x]:
                     pass
             except IndexError:
-                continue
-            direction.append((y, x))
-        if direction == []:
-            continue
-        board_vectors.append(direction)
-    return board_vectors
+                break
 
-def check_path_blocked(vectors: list[list[Vector,]], board: ChessArray, 
+            direction.append((y, x))
+
+        if direction != []:
+            piece_vectors[piece].append(direction)
+
+    return piece_vectors
+
+def check_path_blocked(board: ChessArray, vectors: list[list[Vector,]],  
     oy: int, ox: int
     ) -> list[Vector,]:
     """ Check which vectors are unblocked from the given all_free_vectors list
@@ -160,7 +181,7 @@ def check_path_blocked(vectors: list[list[Vector,]], board: ChessArray,
 
     return unblocked_vectors
         
-def pawn_diag(piece, board, oy, ox) -> list[Vector,]:
+def pawn_diag(board: ChessArray, piece: Piece, oy: int, ox: int) -> list[Vector,]:
     """Returns list of possible capture vectors for a Pawn"""
     capture_vectors = piece.capture_vectors()
     vecs = []
@@ -169,28 +190,23 @@ def pawn_diag(piece, board, oy, ox) -> list[Vector,]:
             newpos = board[oy + cv[0]][ox + cv[1]]
         except IndexError:
             continue
+
         if newpos == '<>':
             continue
+        
         elif newpos.color != piece.color:
             vecs.append(cv)
+        
         else:
-            print('Something happend in fn pawn_diag()')                    # DEBUG
+            print('Something happend in pawn_diag()')                    # DEBUG
     return vecs
-    
 
-def collision_check_knight(piece: Piece, board: ChessArray, ny: int, nx: int
-    ) -> bool:
-    """ Checks if destination has enemy or is empty"""
-    if (board[ny][nx] == '<>' 
-            or board[ny][nx].color != piece.color): 
-        # new pos empty and enemy there             
-        print(f"{piece} no collision {board[ny][nx]}")                      # DEBUG
-        return True
-    else:
-        newpos = lst_idx_to_chess((ny,nx))
-        print(
-            f"{piece} collison on {board[ny][nx]} {newpos}")                # DEBUG
-        return False
+def find_piece(board, player_color, name) -> Coordinate:
+    """ returns first occurence of a piece. Used for finding king """
+    for y in board:
+        for x in y:
+            if x != '<>' and x.name == name and x.color == player_color.lower():
+                return board.index(y), y.index(x)
 
 def highlight_viable_mvs(vectors: tuple[Vector,...], board: ChessArray,
     y: int, x: int,
@@ -200,9 +216,7 @@ def highlight_viable_mvs(vectors: tuple[Vector,...], board: ChessArray,
     for vector in vectors:
         vecy, vecx = vector[0], vector[1]
         board[y + vecy][x + vecx] = "XX"
-    print("HIGHLIGHT VIABLE MOVES")
     draw_board(board)
-
 
 def move_piece(board: ChessArray, ny: int, nx: int, oy: int, ox: int) -> Piece | None:
     board[ny][nx], board[oy][ox] = board[oy][ox], board[ny][nx]
@@ -212,106 +226,130 @@ def move_piece(board: ChessArray, ny: int, nx: int, oy: int, ox: int) -> Piece |
     board[oy].insert(ox, '<>')
     return captured_piece
     
-
 #----------------------------------------------------------------------
 
 def main():
-    # STAGE 0: Initialization
-    current_board = get_start_board()
+    # STAGE 0: Setup
+    board = get_start_board()
+    captured_pieces = {'White': [], 'Black': []}
+    
+    white_pieces = []
+    black_pieces = []
+    for y in board:
+        for x in y:
+            if x != '<>':
+                if x.color == 'white':
+                    white_pieces.append(x)
+                elif x.color == 'black':
+                    black_pieces.append(x)
+                
     while True:
-        caputured_pieces = {'White': [], 'Black': []}
-        turn_counter = 1            # odd White even Black
+        turn_counter = 1            # odd = White / even = Black
         while True:
-            # STAGE 1:          Beginning of Turn
-            draw_board(current_board)
+            # STAGE 1:          Turn Starts
+            draw_board(board)
             if turn_counter % 2 == 1: current_player = 'White'
             else: current_player = 'Black'
 
-            # STAGE 2:          Player Grabs Piece
+            # STAGE 2:          Player Picks Piece
             while True:
                 selected_tile = input(f"{current_player}'s Turn: " )
+                
                 if ipt_checker(selected_tile):
+                    if selected_tile[0].isdigit(): 
+                        selected_tile = selected_tile[1] + selected_tile[0]
                     break
-            
-            if selected_tile[0].isdigit():
-                selected_tile = selected_tile[1] + selected_tile[0]
 
             oy, ox = chess_idx_to_lst(selected_tile)
-            piece: Piece = current_board[oy][ox]
+            piece: Piece = board[oy][ox]
 
-            
-            # STAGE 2.1:        Validating Grabbed Piece
-            if current_board[oy][ox] == '<>':
-                print(f"{selected_tile} is empty, choose another field!")
-                continue
-
-            if current_board[oy][ox].color != current_player.lower():
-                print(f"{current_player}, {piece} does not belong to you!")
+            if not belongs_to_player(board, current_player, oy, ox):
                 continue
             
-            # STAGE 2.2:        Check Where Grabbed Piece Can Move
-            all_vecs = all_board_vectors(piece, current_board, oy, ox)
-            print(f"all_possible_vecs --> {all_vecs}")                      # DEBUG
+            # STAGE 3:          Get All Vectors of All Pieces
+            all_vecs = {}
+            for y, row in enumerate(board):
+                for x, tile in enumerate(row):
+                    if tile == '<>':
+                        continue
 
-            unblocked_vecs = check_path_blocked(all_vecs, current_board, oy, ox)
+                    p = board[y][x]
+                
+                    piece_vectors = free_vectors(board, p, y, x)
+                    unblocked_vecs = check_path_blocked(board, piece_vectors[p], y, x)
 
-            # STAGE 2.3:      Check Pawns Move Diag Move
-            if piece.name == 'Pawn':
-                cap_vectors = pawn_diag(piece, current_board, oy, ox)
-                print(f"Diag Vec: {cap_vectors}")                           # DEBUG
-                if cap_vectors != []:
-                    unblocked_vecs += cap_vectors
+                    # Pawns Capture Vecs
+                    if p.name == 'Pawn':
+                        cap_vectors = pawn_diag(board, p, y, x)
+                        print(f"Diag Vec: {cap_vectors}")                           # DEBUG
+                        
+                        if cap_vectors != []:
+                            unblocked_vecs += cap_vectors
 
-            print(f"these vecs are unblocked: {unblocked_vecs}")            # DEBUG
+                    all_vecs.setdefault(p, unblocked_vecs)
+            
+            for d in all_vecs.items():                                    # DEBUG
+                print(d)
+            print(f">>>> {all_vecs[piece]}")
 
-            if unblocked_vecs == []:
+            if all_vecs[piece] == []:
                 print(f"{piece} can't go anywhere")
                 continue
 
+            # STAGE 3.2:        Highlight Possible Moves        
+            test_board = deepcopy(board)                            # DEBUG 
+            highlight_viable_mvs(all_vecs[piece], test_board, oy, ox)
 
-            test_board = deepcopy(current_board)                            # DEBUG 
-            highlight_viable_mvs(unblocked_vecs, test_board, oy, ox)
-
-            # STAGE 3:          Player declares destination
+            # STAGE 4:          Player Declares Destination
             while True:
                 destination = input(f'Move {piece} ({selected_tile.upper()}) to: ')
+
                 if ipt_checker(destination):
+                    if destination[0].isdigit():
+                        destination = destination[1] + destination[0]
+                    
+                    if selected_tile == destination:
+                        print('You cannot not move!')
+                        continue
+
                     break
-            
-            if destination[0].isdigit():
-                destination = destination[1] + destination[0]
 
             ny, nx = chess_idx_to_lst(destination)
-            vecy, vecx = ny - oy, nx - ox
-            vec = (vecy, vecx)
             
-            # STAGE 3.1:        Validating Move
-            if oy == ny and ox == nx:
-                # Not moving is not legal
-                continue
-
-            # Stage 3.2         Check for Knight
+            # STAGE 5:          Check Move Legality
             if hasattr(piece, "jumps"):
-                if not collision_check_knight(piece, current_board, ny, nx):
+                if belongs_to_player(board, current_player, ny, nx):
                     print(
                         f"{piece} cant mv to {destination.upper()}, one of ur pieces on there"
                         )                                                   # DEBUG
                     continue
 
-            if vec not in unblocked_vecs:
+            vecy = ny - oy 
+            vecx = nx - ox
+
+            if (vecy, vecx) not in all_vecs[piece]:
                 print(f"{piece} cannot move there!")                        # DEBUG
                 continue
 
-            # Stage 4:          Move and Capture
-            capture = move_piece(current_board, ny, nx, oy, ox)
+
+            # STAGE 6:          Move and Capture
+            capture = move_piece(board, ny, nx, oy, ox)
+
             if capture is not None:
-                caputured_pieces[current_player].append(capture)                
+                captured_pieces[current_player].append(capture)                
             
-            print(caputured_pieces.values())                                # DEBUG
+            for i in captured_pieces[current_player]:
+                print(i, end=' ')                             # DEBUG
 
+
+            # STAGE 5
+            # if king_checked(board, current_player):
+            #     print('Check!')
+
+            # Stage 10:          End Step 
             if not piece.first_mv:
-
                 piece.made_first_move()
+
             turn_counter += 1
 
         break
